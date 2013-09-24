6300a8879bcd7542d4683c24da86d4e64265c043
Krum Motsov <krum@athlonsofia.com>
2013-09-24 11:37:27 +0300

    Change name and logo

--- copy_styles.rb
+++ copy_styles.rb
@@ -1,10 +1,10 @@
 # Copy the styles from the master branch to the gh-pages branch
-`mkdir /tmp/jenkins-isotope-style`
-`cp *.css /tmp/jenkins-isotope-style`
+`mkdir /tmp/jenkins-athlon-style`
+`cp *.css /tmp/jenkins-athlon-style`
 `git checkout gh-pages`
 `git fetch origin`
 `git merge origin/gh-pages`
-`cp /tmp/jenkins-isotope-style/*.css ./`
+`cp /tmp/jenkins-athlon-style/*.css ./`
 `git add *.css`
 `git commit -m"Updated styles"`
 `git push origin gh-pages`
